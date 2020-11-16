d3b8263a2ae612e8b64d7afcac892750bb2aa298
XinLing-Yu <103304325@qq.com>
2020-11-16 15:10:01 +0800

    1111

---
+++ 1111-2-1/1111- (2).cpp
@@ -0,0 +1,12 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	char a = 'a';
+//	char b = 'b';
+//	a-=32;
+//	b-=32;
+//	cout <<a <<" " <<b <<endl;
+//	system("pause");
+//	return 0;
+//}
