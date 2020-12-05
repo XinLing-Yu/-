96f943875e000df50a28d3f84d8dd98fb013677a
XinLing-Yu <69040345+XinLing-Yu@users.noreply.github.com>
2020-12-05 20:13:11 +0800

    02.cpp

--- 1202REC/02.cpp
+++ 1202REC/02.cpp
@@ -1,19 +1,19 @@
-//#include<iostream>
-//using namespace std;
-//int main()
-//{
-//	double score;
-//	cin >> score;
-//	if (score >= 60)
-//	{
-//		cout << "Passed!" << endl;
-//		cout << "Yeah!" << endl;
-//	}
-//	else
-//	{
-//		cout << "Failed!" << endl;
-//	}
-//	cout << "BYE!" << endl;
-//	return 0;
-//	system("pause");
-//}
+#include<iostream>
+using namespace std;
+int main()
+{
+	double score;
+	cin >> score;
+	if (score >= 60)
+	{
+		cout << "Passed!" << endl;
+		cout << "Yeah!" << endl;
+	}
+	else
+	{
+		cout << "Failed!" << endl;
+	}
+	cout << "BYE!" << endl;
+	return 0;
+	system("pause");
+}
