e6b4471ad47654cd7a4882aa7873c2c4f9e8b722
XinLing-Yu <103304325@qq.com>
2020-12-05 20:06:33 +0800

    1116

---
+++ 1116-2-3/1116-2 (4).cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char c1,c2,c3,c4,c5;
+	c1='C';
+	c2='h';
+	c3='i';
+	c4='n';
+	c5='a';
+	cout<<char(c1+4)<<char(c2+4)<<char(c3+4)<<char(c4+4)<<char(c5+4)<<endl;
+	return 0;
+} 
